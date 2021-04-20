db75535b0bbfa6cf384c7de34c1779a00187c0fd
Jerry Macedo Castro <leom551@hotmail.com>
2021-04-20 20:54:55 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-use-before-define
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello darkness my old friend</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
